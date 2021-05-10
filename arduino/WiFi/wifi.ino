79204c8ce7c6020eadbbccf5730f9b5fc792a648
Walter Ocampo <fearless347@gmail.com>
2021-05-09 19:57:39 -0500

    Tareas

--- arduino/WiFi/wifi.ino
+++ arduino/WiFi/wifi.ino
@@ -1,33 +1,117 @@
 #include <SoftwareSerial.h>
 #define DEBUG true
 SoftwareSerial esp8266(4,5); // make RX Arduino line is pin 5, make TX Arduino line is pin 6.
+
+float valorTemperatura = 0;
+float valorConcentracionGas = 0;
+
+const int LeftMotorForward = 13;
+const int LeftMotorBackward = 12;
+const int RightMotorForward = 11;
+const int RightMotorBackward = 10;
+
+boolean goesForward = false;
     
 void setup()
 {
     Serial.begin(9600);
+    pinMode(RightMotorForward, OUTPUT);
+    pinMode(LeftMotorForward, OUTPUT);
+    pinMode(LeftMotorBackward, OUTPUT);
+    pinMode(RightMotorBackward, OUTPUT);
     configurarWifi();
+}
+
+void moveStop()
+{
+
+  digitalWrite(RightMotorForward, LOW);
+  digitalWrite(LeftMotorForward, LOW);
+  digitalWrite(RightMotorBackward, LOW);
+  digitalWrite(LeftMotorBackward, LOW);
+}
+
+void moveForward()
+{
+  Serial.print("Avvanzando");
+  if (!goesForward)
+  {
+  
+    goesForward = true;
+
+    digitalWrite(LeftMotorForward, HIGH);
+    digitalWrite(RightMotorForward, HIGH);
+
+    digitalWrite(LeftMotorBackward, LOW);
+    digitalWrite(RightMotorBackward, LOW);
+  }
+}
+
+void moveBackward()
+{
+
+  goesForward = false;
+
+  digitalWrite(LeftMotorBackward, HIGH);
+  digitalWrite(RightMotorBackward, HIGH);
+
+  digitalWrite(LeftMotorForward, LOW);
+  digitalWrite(RightMotorForward, LOW);
+}
+
+void turnRight()
+{
+
+  digitalWrite(LeftMotorForward, HIGH);
+  digitalWrite(RightMotorBackward, HIGH);
+
+  digitalWrite(LeftMotorBackward, LOW);
+  digitalWrite(RightMotorForward, LOW);
+
+  delay(500);
+
+  digitalWrite(LeftMotorForward, HIGH);
+  digitalWrite(RightMotorForward, HIGH);
+
+  digitalWrite(LeftMotorBackward, LOW);
+  digitalWrite(RightMotorBackward, LOW);
+}
+
+void turnLeft()
+{
+
+  digitalWrite(LeftMotorBackward, HIGH);
+  digitalWrite(RightMotorForward, HIGH);
+
+  digitalWrite(LeftMotorForward, LOW);
+  digitalWrite(RightMotorBackward, LOW);
+
+  delay(500);
+
+  digitalWrite(LeftMotorForward, HIGH);
+  digitalWrite(RightMotorForward, HIGH);
+
+  digitalWrite(LeftMotorBackward, LOW);
+  digitalWrite(RightMotorBackward, LOW);
 }
 
  void configurarWifi()
  {
-    esp8266.begin(115200); // your esp's baud rate might be different
-    enviarComando("AT+RST\r\n",2000,DEBUG); // reset module
-//    enviarComando("AT+CWMODE=2\r\n",1000,DEBUG); // configure as Access point mode
-//    enviarComando("AT+CWSAP=\"AccessPointname\",\"password\",1,4\r\n",1000,DEBUG); //this sets access point name and password //[channel -> 1 to 10] , [securitytype -> 0 – Open / 2 – WPA_PSK /3 – WPA2_PSK / 4 – WPA_WPA2_PSK ]
-    enviarComando("AT+CWMODE=3\r\n",1000,DEBUG); // configure as Wireless Station mode
-    enviarComando("AT+CWJAP=\"CLARO-B612-6417\",\"10gFTNtLm2\"\r\n", 6000, DEBUG); //Put Your SSID and password if activate as Station mode else comment down the line
-    enviarComando("AT+CIFSR\r\n",2000,DEBUG); // get ip address
-    enviarComando("AT+CIPMUX=1\r\n",1000,DEBUG); // configure for multiple connections
-    enviarComando("AT+CIPSERVER=1,80\r\n",1000,DEBUG); // turn on server on port 80
+//    esp8266.begin(115200); // your esp's baud rate might be different
+//    enviarComando("AT+RST\r\n",2000,DEBUG); // reset module
+////    enviarComando("AT+CWMODE=2\r\n",1000,DEBUG); // configure as Access point mode
+////    enviarComando("AT+CWSAP=\"AccessPointname\",\"password\",1,4\r\n",1000,DEBUG); //this sets access point name and password //[channel -> 1 to 10] , [securitytype -> 0 – Open / 2 – WPA_PSK /3 – WPA2_PSK / 4 – WPA_WPA2_PSK ]
+//    enviarComando("AT+CWMODE=3\r\n",1000,DEBUG); // configure as Wireless Station mode
+//    enviarComando("AT+CWJAP=\"CLARO-B612-6417\",\"10gFTNtLm2\"\r\n", 6000, DEBUG); //Put Your SSID and password if activate as Station mode else comment down the line
+//    enviarComando("AT+CIFSR\r\n",2000,DEBUG); // get ip address
+//    enviarComando("AT+CIPMUX=1\r\n",1000,DEBUG); // configure for multiple connections
+//    enviarComando("AT+CIPSERVER=1,80\r\n",1000,DEBUG); // turn on server on port 80
  }
 void moduloWifi()
 {
     if(esp8266.available()) {
         if(esp8266.find("+IPD,")) {
-            // subtract 48 because the read() function returns 
-            // the ASCII decimal value and 0 (the first decimal number) starts at 48
-            int idConexion = esp8266.read() - 48; 
-            //To read the url sent by the client
+            int idConexion = esp8266.read()-48;
             String mensaje;
             esp8266.find("?");
             delay(100);
@@ -38,26 +122,35 @@ void moduloWifi()
                 String json = "{ \"temperatura\": \"";
                 json += valorTemperatura;
                 json += "\"}";
-                String cipSend = "AT+CIPSEND=";
-                cipSend += idConexion;
-                cipSend += ",";
-                cipSend += json.length();
-                cipSend += "\r\n";
-                enviarComando(cipSend,200,DEBUG);
-                enviarComando(json,200,DEBUG);
-                //BELOW THIS LINE CLOSE THE CONNECTION
-                String comandoCierre = "AT+CIPCLOSE="; 
-                comandoCierre += idConexion; // append connection id
-                comandoCierre += "\r\n";
-                enviarComando(comandoCierre,300,DEBUG);
+                enviarRespuesta(json, idConexion);
             }
 
             if (comando == "G") {
-              Serial.print("Adelante");
+                Serial.print("Adelante");
+//                moveForward();
+                String json = "{ \"respuesta\": \"ok\"}";
+                enviarRespuesta(json, idConexion);
+//                delay(3000);
             }
             
         }
     }
+}
+
+void enviarRespuesta(String json, int idConexion)
+{
+    String cipSend = "AT+CIPSEND=";
+    cipSend += idConexion;
+    cipSend += ",";
+    cipSend += json.length();
+    cipSend += "\r\n";
+    enviarComando(cipSend,200,DEBUG);
+    enviarComando(json,200,DEBUG);
+    //BELOW THIS LINE CLOSE THE CONNECTION
+    String comandoCierre = "AT+CIPCLOSE="; 
+    comandoCierre += idConexion; // append connection id
+    comandoCierre += "\r\n";
+    enviarComando(comandoCierre,300,DEBUG);
 }
 
 void enviarComando(String comando, const int duracion, boolean debug)
@@ -75,5 +168,18 @@ void enviarComando(String comando, const int duracion, boolean debug)
 
 void loop()
 {
-    moduloWifi();
+//      moduloWifi();
+      digitalWrite(LeftMotorForward, HIGH);
+      digitalWrite(LeftMotorBackward, LOW);
+    
+      digitalWrite(RightMotorForward, HIGH);
+      digitalWrite(RightMotorBackward, LOW);
+      delay(5000);
+    
+      digitalWrite(LeftMotorForward, LOW);
+      digitalWrite(LeftMotorBackward, HIGH);
+    
+      digitalWrite(RightMotorForward, LOW);
+      digitalWrite(RightMotorBackward, HIGH);
+      delay(5000);
 }
